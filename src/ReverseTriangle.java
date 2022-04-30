c942d44a87ad08f25c3925f03b27d46e8a8486a3
onur koc <onurkottz@gmail.com>
2022-04-30 12:59:04 +0300

    Some

--- src/ReverseTriangle.java
+++ src/ReverseTriangle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class reverseTriangle {
+public class ReverseTriangle {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
